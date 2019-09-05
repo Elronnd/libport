ff4764796cfe831732ac181744c1b6eed545757f
Elronnd <elronnd@elronnd.net>
2019-09-05 11:32:34 -0700

    Misc cleanup; apparently I broke the magic fairy juice; clargs are alloca'd; apparently having multiple states doesn't work so put a note in the example about that

--- src/example.c
+++ src/example.c
@@ -1,12 +1,17 @@
 #include "port.h"
 
 int main(void) {
-	P6State *state = p6_init();
+	P6State *state1 = p6_init();
+	//P6State *state2 = p6_init();
+	// ^^ that breaks, for some reason?
 
-	p6eval(state, "say 5;");
-	p6eval(state, "say 5;");
-	p6eval(state, "say 5;");
+	p6eval(state1, "$scratch = 19");
+	/*p6eval(state2, "$scratch = 22");
+	 *p6eval(state2, "say $scratch");
+	 */
+	p6eval(state1, "say $scratch");
 
-	p6_deinit(state);
+	//p6_deinit(state2);
+	p6_deinit(state1);
 	return 0;
 }
--- src/port.c
+++ src/port.c
@@ -23,13 +23,14 @@ static void toplevel_initial_invoke(MVMThreadContext *tc, void *data) {
 	MVM_frame_invoke(tc, (MVMStaticFrame *)data, MVM_callsite_get_common(tc, MVM_CALLSITE_ID_NULL_ARGS), NULL, NULL, NULL, -1);
 }
 
-void set_evaluator(P6State *state, void(*evaluator)(char*)) {
+static void set_evaluator(P6State *state, void(*evaluator)(char*)) {
 	state->evaluator = evaluator;
 }
 
 
-static char * const MAGIC_FAIRY_JUICE = "USE NATIVECALL;\
+static char * const MAGIC_FAIRY_JUICE = "use NativeCall;\
 use MONKEY-SEE-NO-EVAL;\
+my $scratch = 17;\
 sub evaluate(Str $x) { EVAL $x; };\
 my &set-evaluator = nativecast(:(Pointer, &callback (Str)), Pointer.new(+@*ARGS[0]));\
 &set-evaluator(Pointer.new(+@*ARGS[1]), &evaluate);";
@@ -42,22 +43,14 @@ P6State *p6_init(void) {
 		return NULL;
 	}
 
-	const char *lib_path[4];
-	char *raw_clargs[4];
-
 	MVM_crash_on_error();
 
 	ret->instance = MVM_vm_create_instance();
 
-	size_t num_lib_paths = 0;
-	lib_path[num_lib_paths++] = PERL6_INSTALL_PATH "/share/nqp/lib";
-	lib_path[num_lib_paths++] = PERL6_INSTALL_PATH "/share/perl6/lib";
-	lib_path[num_lib_paths++] = PERL6_INSTALL_PATH "/share/perl6/runtime";
-	lib_path[num_lib_paths++] = NULL;
-
-	for (size_t i = 0; i < num_lib_paths; i++) {
-		ret->instance->lib_path[i] = lib_path[i];
-	}
+	ret->instance->lib_path[0] = PERL6_INSTALL_PATH "/share/nqp/lib";
+	ret->instance->lib_path[1] = PERL6_INSTALL_PATH "/share/perl6/lib";
+	ret->instance->lib_path[2] = PERL6_INSTALL_PATH "/share/perl6/runtime";
+	ret->instance->lib_path[3] = NULL;
 
 	// stash the rest of the raw command line args in the instance
 	ret->instance->prog_name  = PERL6_INSTALL_PATH "/share/perl6/runtime/perl6.moarvm";
@@ -81,16 +74,16 @@ P6State *p6_init(void) {
 		});
 
 	ret->instance->num_clargs = 4;
+
+	char *raw_clargs[4];
 	raw_clargs[0] = "-e";
 	raw_clargs[1] = MAGIC_FAIRY_JUICE;
 
-	static char buf[30];
-	sprintf(buf, "%zu", (uintptr_t)set_evaluator);
-	raw_clargs[2] = buf;
+	raw_clargs[2] = alloca(32);
+	sprintf(raw_clargs[2], "%zu", (uintptr_t)set_evaluator);
 
-	static char buf2[30];
-	sprintf(buf2, "%zu", (uintptr_t)ret);
-	raw_clargs[3] = buf2;
+	raw_clargs[3] = alloca(32);
+	sprintf(raw_clargs[3], "%zu", (uintptr_t)ret);
 
 
 	ret->instance->raw_clargs = raw_clargs;
